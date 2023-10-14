9cd4e4b8d1f194e3b9adbf94e038c9137f0a9069
asbdu63a1223032 <144726973+asbdu63a1223032@users.noreply.github.com>
2023-10-14 21:51:24 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-# Python 3 program to find
-# factorial of given number
-def factorial(n):
-  if n < 0:
-    return 0
-  elif n == 0 or n == 1:
-    return 1
-  else:
-    fact = 1
-    while (n > 1):
-      fact *= n
-      n -= 1
-    return fact
+def linearSearchProduct(productlist, targetProduct):
+    indices = []
+    
+    for index, product in enumerate(productlist):
+        if product == targetProduct:
+            indices.append(index)
+    
+    return indices
 
-
-# Driver Code
-num = 5
-print("Factorial of", num, "is", factorial(num))
-
-# This code is contributed by Dharmik Thakkar
+products = ["shoes", "boot", "Loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2= " apple"
+result = linearSearchProduct(products, target)
+print(result)
